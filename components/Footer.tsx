ba2d78ac5b520eb8fc4e1b47d200a6a835341fea
ananya5254 <79458816+ananya5254@users.noreply.github.com>
2023-05-15 11:43:31 +0530

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer style={{ position: "absolute", bottom: 0, width:"100%",backgroundColor:"black",color:"white" }}>
+        <p>&copy; Newspaper WordPress Theme by TagDiv</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { FaInstagramSquare } from 'react-icons/fa';
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">MeZinkBlog</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+          <a href="https://www.instagram.com/your-instagram-handle">
+            <FaInstagramSquare />
+          </a>
+        </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
