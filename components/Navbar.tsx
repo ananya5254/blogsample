5f2cf104392d4525f1000ee7c65afea55796c893
ananya5254 <79458816+ananya5254@users.noreply.github.com>
2023-05-23 13:06:04 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,66 +1,55 @@
-import Link from "next/link";
-import React from "react";
-import { FaInstagramSquare } from 'react-icons/fa';
-import { useState } from 'react';
-const Navbar=() => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
- 
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
- 
-
+import React from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">MeZinkBlog</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">MEZINK</Link>
-          </li>
-          <li className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
-          <a href="#" onClick={toggleDropdown}>
-            TIPS & TUTORIALS
-          </a>
-          {isDropdownOpen && (
-            <ul className="dropdown-menu">
-              <li>
-                <a href="/dropdown1">Tutorial Dashboard YouTube</a>
-              </li>
-              <li>
-                <a href="/dropdown2">Tutorial Figma</a>
-              </li>
-              <li>
-                <a href="/dropdown3">Mezink Academy</a>
-              </li>
-              <li>
-                <a href="/dropdown3">Mezink Academy</a>
-              </li>
-              <li>
-                <a href="/dropdown3">Mezink Academy</a>
-              </li>
-              <li>
-                <a href="/dropdown3">Perpustakaan Konten Kreator</a>
-              </li>
-
-            </ul>
-          )}
-          </li>
-          <li>
-            <Link href="/about">KRITIK & SARAN</Link>
-          </li>
-          <li>
-          <a href="https://www.instagram.com/your-instagram-handle">
-            <FaInstagramSquare />
-          </a>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>MeZinkBlog</div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="https://mez.ink/">
+            <a>MeZink</a>
+          </Link>
         </li>
-        </ul>
-      </div>
-    </div>
-    
+        <li className={styles.dropdown}>
+          <a href="#">Tips & Tutorials</a>
+          <ul className={styles.dropdownContent}>
+            <li>
+              <Link href="/dropdown-link1">
+                <a>Tutorial Dashboard YouTube</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/dropdown-link2">
+                <a>Tutorial Figma</a>
+              </Link>
+              <Link href="/dropdown-link3">
+                <a>MeZink Academy</a>
+              </Link>
+              <Link href="/dropdown-link4">
+                <a>Tutorial Figma</a>
+              </Link>
+              <Link href="/dropdown-link5">
+                <a>Perpustakaan Konten Kreator</a>
+              </Link>
+              <Link href="/dropdown-link6">
+                <a>Inspirasi Untuk Konten Kreator</a>
+              </Link>
+              <Link href="/dropdown-link7">
+              <a>Sosmed & Komunitas</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link href="/https://mez.ink/">
+            <a>KIRITK & SARAN </a>
+          </Link>
+        </li>
+      </ul>
+     
+    </nav>
   );
-}
+};
+
 export default Navbar;
